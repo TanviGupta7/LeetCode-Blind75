78dd42515bcc7007880edd075bcc034a4f0c8331
Tanvi Gupta <guptanvi7623@gmail.com>
2025-02-24 01:30:28 +0530

    Time: 0 ms (100%), Space: 40.5 MB (49.34%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,3 +1,4 @@
+ //Optimal using DP
 class Solution {
     public int climbStairs(int n) {
         
@@ -16,3 +17,20 @@ class Solution {
         return two;
     }
 }
+/*
+//Naive- using recursion
+public class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
+*/
