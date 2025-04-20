ed7192f0c664fc4771a662a83e71557040b9ac4d
Tanvi Gupta <guptanvi7623@gmail.com>
2025-04-21 01:17:36 +0530

    Time: 0 ms (100%), Space: 42.2 MB (71.4%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.java
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.java
@@ -1,26 +1,17 @@
-import java.util.*;
-
 class Solution {
     public int numRabbits(int[] answers) {
-        Map<Integer, Integer> countMap = new HashMap<>();
-        int totalRabbits = 0;
 
-        // Count how many rabbits gave each answer
-        for (int ans : answers) {
-            countMap.put(ans, countMap.getOrDefault(ans, 0) + 1);
+        int n = answers.length;
+        int[] count = new int[1000];
+        
+        int ans = 0;
+        for(int x:answers){
+            if(++count[x] == 1)
+                ans += x + 1;
+            if(count[x] == x + 1)
+                count[x] = 0;
         }
-
-        // Process each group of answers
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            int k = entry.getKey();         // rabbit's answer
-            int count = entry.getValue();   // number of rabbits who gave that answer
-
-            int groupSize = k + 1;
-            int numberOfGroups = (int) Math.ceil((double) count / groupSize);
-
-            totalRabbits += numberOfGroups * groupSize;
-        }
-
-        return totalRabbits;
+        
+        return ans;
     }
 }
