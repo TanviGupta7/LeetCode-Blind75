63f1a725e167af626c5fad5b544fa99111b2910a
Tanvi Gupta <guptanvi7623@gmail.com>
2025-05-03 19:54:42 +0530

    Time: 0 ms (100%), Space: 40.7 MB (18.48%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,20 +1,15 @@
 class Solution {
     public int fib(int n) {
-        return series(n);
-    }
-    public int series(int n){
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return series(n-1)+series(n-2);
+        int a = 0, b = 1;
+
+        if (n == 0) return a;
+        if (n == 1) return b;
+
+        for (int i = 2; i <= n; i++) {
+            int temp = b;
+            b = a + b; // let b update its value first and then a gets updated
+            a = temp;
+        }
+        return b;
     }
 }
-
-/* if(n==0) return 0;
-        if(n==1) return 1;
-        int a=0,b=1,c=0;
-        for(int i=2;i<=n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return c;*/
