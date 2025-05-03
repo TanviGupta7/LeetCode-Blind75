29357e2b8139dc815f484b1bc93c1fcd9507cad1
Tanvi Gupta <guptanvi7623@gmail.com>
2025-05-03 19:39:01 +0530

    Time: 9 ms (23.34%), Space: 41 MB (6.5%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,6 +1,15 @@
 class Solution {
     public int fib(int n) {
+        return series(n);
+    }
+    public int series(int n){
         if(n==0) return 0;
+        if(n==1) return 1;
+        return series(n-1)+series(n-2);
+    }
+}
+
+/* if(n==0) return 0;
         if(n==1) return 1;
         int a=0,b=1,c=0;
         for(int i=2;i<=n;i++){
@@ -8,6 +17,4 @@ class Solution {
             a=b;
             b=c;
         }
-        return c;
-    }
-}
+        return c;*/
