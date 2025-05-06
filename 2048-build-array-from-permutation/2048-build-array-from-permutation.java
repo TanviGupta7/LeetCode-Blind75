20fe9e5546d32736c43caf0d192913031b8966fd
Tanvi Gupta <guptanvi7623@gmail.com>
2025-05-07 00:24:46 +0530

    Time: 2 ms (7.3%), Space: 45.5 MB (27.48%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -2,10 +2,14 @@ class Solution {
 
     public int[] buildArray(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
+        // Build the final value on the first iteration
         for (int i = 0; i < n; ++i) {
-            ans[i] = nums[nums[i]];
+            nums[i] += 1000 * (nums[nums[i]] % 1000);
         }
-        return ans;
+        // Modified to final value on the second iteration
+        for (int i = 0; i < n; ++i) {
+            nums[i] /= 1000;
+        }
+        return nums;
     }
 }
