7433dfa88809662ed0f6412ba1b25e11c7113294
Tanvi Gupta <guptanvi7623@gmail.com>
2025-05-16 00:20:32 +0530

    Time: 1 ms (96.15%), Space: 45 MB (40.89%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.java
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.java
@@ -1,16 +1,13 @@
 class Solution {
     public List<String> getLongestSubsequence(String[] words, int[] groups) {
-        List<String> result = new ArrayList<>();
-        int prev = -1;
-
-        for (int i = 0; i < groups.length; i++) {
-            if (groups[i] != prev) {
-                prev = groups[i];
-                result.add(words[i]);
+        List<String> res = new ArrayList<>();
+        res.add(words[0]);
+        for (int i = 1; i < groups.length; ++i) {
+            if (groups[i] != groups[i - 1]) {
+                res.add(words[i]);
             }
         }
-
-        return result;
+        return res;
     }
 }
 /*class Solution {
