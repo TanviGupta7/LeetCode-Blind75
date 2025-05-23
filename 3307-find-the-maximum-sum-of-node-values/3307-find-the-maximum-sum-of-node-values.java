78bcc098de10f1008166ed93c14157438614d226
Tanvi Gupta <guptanvi7623@gmail.com>
2025-05-24 01:26:08 +0530

    Time: 2 ms (76.32%), Space: 54.7 MB (64.47%) - LeetHub

--- 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
         long[][] memo = new long[nums.length][2];
         for (long[] row : memo) {
@@ -25,5 +25,34 @@ class Solution {
 
         // Memoize and return the result
         return memo[index][isEven] = Math.max(xorDone, noXorDone);
+    }
+} */
+import java.util.*;
+
+class Solution {
+    public long maximumValueSum(int[] v, int k, int[][] edges) {
+        long total = 0;
+        for (int val : v) {
+            total += val;
+        }
+
+        long totalDiff = 0;
+        int positiveCount = 0;
+        long minAbsDiff = Integer.MAX_VALUE;
+
+        for (int val : v) {
+            long diff = (val ^ k) - val;
+            if (diff > 0) {
+                totalDiff += diff;
+                positiveCount++;
+            }
+            minAbsDiff = Math.min(minAbsDiff, Math.abs(diff));
+        }
+
+        if (positiveCount % 2 == 1) {
+            totalDiff -= minAbsDiff;
+        }
+
+        return total + totalDiff;
     }
 }
