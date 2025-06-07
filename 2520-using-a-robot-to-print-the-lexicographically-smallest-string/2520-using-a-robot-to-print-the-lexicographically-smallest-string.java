cd4679fb62064fe6043a24ce270181977b18d7bf
Tanvi Gupta <guptanvi7623@gmail.com>
2025-06-08 00:57:30 +0530

    Time: 186 ms (18.36%), Space: 55.5 MB (38.61%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.java
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.java
@@ -1,34 +1,24 @@
 class Solution {
     public String robotWithString(String s) {
-        int[] freq = new int[26]; // Frequency of characters remaining in s
-        for (char ch : s.toCharArray()) {
-            freq[ch - 'a']++;
-        }
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) freq[c - 'a']++;
 
-        Stack<Character> stack = new Stack<>(); // Represents string t
-        StringBuilder result = new StringBuilder(); // Final written string p
-        char minChar = 'a'; // Current smallest character in remaining s
+        StringBuilder res = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        char min = 'a';
 
-        for (char ch : s.toCharArray()) {
-            stack.push(ch); // Perform first operation (move to t)
-            freq[ch - 'a']--; // Decrease frequency as it's removed from s
+        for (char c : s.toCharArray()) {
+            stack.push(c);
+            freq[c - 'a']--;
 
-            // Update minChar to the next available smallest character
-            while (minChar <= 'z' && freq[minChar - 'a'] == 0) {
-                minChar++;
-            }
+            while (min <= 'z' && freq[min - 'a'] == 0) min++;
 
-            // Perform second operation as long as top of t <= smallest left in s
-            while (!stack.isEmpty() && stack.peek() <= minChar) {
-                result.append(stack.pop()); // Write to paper
+            while (!stack.isEmpty() && stack.peek() <= min) {
+                res.append(stack.pop());
             }
         }
 
-        // Write any remaining characters in t
-        while (!stack.isEmpty()) {
-            result.append(stack.pop());
-        }
-
-        return result.toString();
+        while (!stack.isEmpty()) res.append(stack.pop());
+        return res.toString();
     }
 }
