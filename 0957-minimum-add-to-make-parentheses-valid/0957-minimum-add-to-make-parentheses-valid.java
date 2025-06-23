a0d7f7791f00a774c81c86846b6f6fceee20dfef
Tanvi Gupta <guptanvi7623@gmail.com>
2025-06-23 18:26:05 +0530

    Time: 0 ms (100%), Space: 41.2 MB (96.17%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,5 +1,30 @@
 class Solution {
     public int minAddToMakeValid(String s) {
+        int c1=0,c2=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(')
+            c1++;
+            else if(s.charAt(i)==')'){
+                if(c1>0)
+                c1--;
+                else
+                c2++;
+            }
+        }
+        return c2+c1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+        /*
         Stack<Character> st=new Stack<Character>();
         int c=0,c1=0;
         if(s.isEmpty()==true)
@@ -21,4 +46,4 @@ class Solution {
         return st.size();
         
     }
-}
+}*/
