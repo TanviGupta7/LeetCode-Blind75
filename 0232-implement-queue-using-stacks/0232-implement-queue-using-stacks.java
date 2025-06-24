b186310603ce904e3378c78357859e9c21fabd41
Tanvi Gupta <guptanvi7623@gmail.com>
2025-06-24 19:18:57 +0530

    Time: 4 ms (0.24%), Space: 41.7 MB (19.05%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -4,11 +4,65 @@ class MyQueue {
     Stack<Integer> input = new Stack<>();
     Stack<Integer> output = new Stack<>();
 
+    /** Initialize your data structure here. */
+    public MyQueue() {
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        System.out.println("The element pushed is " + x);
+        input.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.pop());
+            }
+        }
+
+        if (!output.empty()) {
+            return output.pop();
+        } else {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.pop());
+            }
+        }
+
+        if (!output.empty()) {
+            return output.peek();
+        } else {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty(); // ✅ fixed
+    }
+}
+
+
+
+/*class MyQueue {
+    Stack<Integer> input = new Stack<>();
+    Stack<Integer> output = new Stack<>();
+
     public MyQueue() {
         // No need for front and rear
     }
 
-    public void push(int x) {
+    public void push(int x) { //O(N)
         // Move all elements to output
         while (!input.isEmpty()) {
             output.push(input.pop());
