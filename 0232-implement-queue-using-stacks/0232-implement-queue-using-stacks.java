5033c044a35706494d31a677027d920adbb4b7ec
Tanvi Gupta <guptanvi7623@gmail.com>
2025-06-26 13:33:45 +0530

    Time: 0 ms (100%), Space: 41.4 MB (52.15%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,8 +1,8 @@
 import java.util.Stack;
 
 class MyQueue {
-    Stack<Integer> input = new Stack<>();
-    Stack<Integer> output = new Stack<>();
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
 
     /** Initialize your data structure here. */
     public MyQueue() {
@@ -10,45 +10,30 @@ class MyQueue {
 
     /** Push element x to the back of queue. */
     public void push(int x) {
-        System.out.println("The element pushed is " + x);
-        input.push(x);
+        while(s1.size()!=0){
+            s2.push(s1.peek());
+            s1.pop();
+        }
+        s1.push(x);
+        while(s2.size()!=0){
+            s1.push(s2.peek());
+            s2.pop();
+        }
     }
 
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        if (output.empty()) {
-            while (!input.empty()) {
-                output.push(input.pop());
-            }
-        }
-
-        if (!output.empty()) {
-            return output.pop();
-        } else {
-            System.out.println("Queue is empty");
-            return -1;
-        }
+       return s1.pop();
     }
 
     /** Get the front element. */
     public int peek() {
-        if (output.empty()) {
-            while (!input.empty()) {
-                output.push(input.pop());
-            }
-        }
-
-        if (!output.empty()) {
-            return output.peek();
-        } else {
-            System.out.println("Queue is empty");
-            return -1;
-        }
+        return s1.peek();
     }
 
     /** Returns whether the queue is empty. */
     public boolean empty() {
-        return input.isEmpty() && output.isEmpty(); // ✅ fixed
+        return s1.isEmpty() && s2.isEmpty(); 
     }
 }
 
